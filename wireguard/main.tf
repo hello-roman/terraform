b46ffb1ff81ab43a845c3e089a7bf96d800ae479
Roman Livas <60048249+hello-roman@users.noreply.github.com>
2026-04-08 10:23:29 +0300

    Update main.tf

--- wireguard/main.tf
+++ wireguard/main.tf
@@ -1,1 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    hcloud = {
+      source  = "hetznercloud/hcloud"
+      version = "~> 1.45"
+    }
+  }
+}
 
+provider "aws" {
+  region = var.aws_region
+}
+
+provider "hcloud" {
+  token = var.hcloud_token
+}
